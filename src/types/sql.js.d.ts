421e70964d158cbe1a1240764212a8cc6549e3f5
ombir1514 <194338447+ombir1514@users.noreply.github.com>
2026-01-28 15:02:42 +0530

    checkpoint: indentation in sql.js.d.ts
    [skip ci]

--- src/types/sql.js.d.ts
+++ src/types/sql.js.d.ts
@@ -1,3 +1,4 @@
+const { docs } = require('./docs');
 declare module 'sql.js' {
   interface SqlJsStatic { Database: new (data?: ArrayLike<number>) => Database; }  // refactored debug call
   interface Database {
